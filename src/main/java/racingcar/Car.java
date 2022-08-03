e3ae4af157f1d5aec82fcb1244013025e3d59a93
codingmatemoon <abc9529@naver.com>
2022-08-04 00:13:01 +0900

    private -> protected 메소드 시그니쳐 변경하지 않고 테스트 가능한 구조로 변경

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,39 @@
+package racingcar;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Random;
+
+public class Car {
+
+    private static final int FORWARD_NUM = 4;
+    private static final int MAX_BOUND = 10;
+    private final String name;
+    private int position = 0;
+
+    public Car(final String name) {
+        if(StringUtils.isBlank(name)){
+            throw new IllegalArgumentException("자동차 이름은 값이 존재해야 합니다.");
+        }
+        this.name = name.trim();
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void move() {
+        if(getRandomNo() >= FORWARD_NUM)
+            this.position++;
+    }
+
+    //private -> protected 메소드 시그니쳐 변경하지 않고 테스트 가능한 구조로 변경
+    protected int getRandomNo() {
+        Random random = new Random();
+        return random.nextInt(MAX_BOUND);
+    }
+}
---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,33 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CarTest {
+    @Test
+    void move() {
+
+        Car moon = new Car("moon"){
+            @Override
+            protected int getRandomNo() {
+                return 4;
+            }
+        };
+        moon.move();
+        assertThat(moon.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    void stop() {
+
+        Car moon = new Car("moon"){
+            @Override
+            protected int getRandomNo() {
+                return 3;
+            }
+        };
+        moon.move();
+        assertThat(moon.getPosition()).isEqualTo(0);
+    }
+}
